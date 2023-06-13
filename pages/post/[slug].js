9697b3804b3a69fdb7bb4daea56cde739f72eb9f
Adhurim4 <adhuriim.muqiqi@gmail.com>
2023-06-13 13:24:13 +0200

    .env to gitignore

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import { useRouter } from 'next/router';
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
  
 
 const PostDetails = ({ post }) => {
- 
+
+  const router = useRouter();
+  
+  if(router.isFallback){
+    return <Loader />
+  }
+
   return (
     <>
       <div className="container mx-auto px-10 mb-8">
